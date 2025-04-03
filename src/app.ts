6741db9786fe907aa8ca6d1914ce25b450992cf9
Javohir Ismatullayev <ismatullayevpro@gmail.com>
2025-04-03 15:49:29 +0000

    Add ssl

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,18 @@
 import express from 'express';
 import path from 'path';
 import router from './routes';
+import https from 'https';
+import fs from 'fs';
+import dotenv from 'dotenv';
+
+dotenv.config()
 
 const app = express()
 const port = process.env.PORT || 3000;
+const options = {
+  key: fs.readFileSync(process.env.PRIVATE_KEY_PATH || ''),
+  cert: fs.readFileSync(process.env.FULLCHAIN_KEY_PATH || ''),
+}
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -12,6 +21,6 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/', router);
 
-app.listen(port, () => {
+https.createServer(options, app).listen(port, () => {
   console.log(`Flashcards app listening on port ${port}`)
 })
